1fac10c328770643b071ba0197ec8acfdee8f953
MuhammadYusuf07 <yusuftkr077@gmail.com>
2023-05-30 20:20:38 +0700

    initial commit

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -1,25 +1,61 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\Employee;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
+    //
     public function index(){
-
         $data = Employee::all();
-        return view('datapegawai',compact('data'));
+  //      dd($data);
+        return view('datapegawai',compact('data')); 
     }
-
-    public function tambahpegawai(){
+    public function tambahpegawai()
+    {
         return view('tambahdata');
     }
+    public function insertdata(Request $request)
+    {
+       // dd($request->all());
+       $data = Employee::create($request->all());
 
-    public function insertdata(Request $request){
-        // dd($request->all());
-        Employee::create($request->all());
-        return redirect()->route('pegawai')->with('success','Data Berhasil Di Tambahkan');
+       if ($request->hasFile('foto')) {
+           $request->file('foto')->move('fotopegawai/', $request->file('foto')->getClientOriginalName()); 
+           $data->foto = $request->file('foto')->getClientOriginalName();
+           $data->save();
+       } 
+       return redirect()->route('pegawai')->with('success', 'Data Berhasil Ditambahkan');
+       
     }
+    public function tampilkandata($id)
+    {
+        $data = Employee::find($id);
+        //dd($data);
+        return view('tampildata', compact('data'));
+    }
+    public function updatedata(Request $request, $id)
+    {
+        $data = Employee::find($id);
+        $data->update($request->all());
+
+        if ($request->hasFile('foto')) {
+            $request->file('foto')->move('fotopegawai/', $request->file('foto')->getClientOriginalName());
+            $data->foto = $request->file('foto')->getClientOriginalName();
+            $data->save();
+        }
+
+        return redirect()->route('pegawai')->with('success', 'Data Berhasil Diupdate');
+    }
+
+    public function delete($id)
+    {
+        $data = Employee::find($id);
+        $data->delete();
+        return redirect()->route('pegawai')->with('success','Data Berhasil Dihapus');
+    }
+
+ 
 }
